0ab47b7ce827654443842e532bad986972762bc5
Parv Prajapati <158603903+parv224@users.noreply.github.com>
2026-02-02 12:16:19 +0530

    Fix manifest and clean App Store metadata

--- estate_property_offers/__manifest__.py
+++ estate_property_offers/__manifest__.py
@@ -3,11 +3,13 @@
     "summary": "Manage property listings and offers",
     "version": "18.0.1.0",
     "author": "Odoo PS",
-    "website": "http://www.odoo.com",
+    "website": "https://www.odoo.com",
     "license": "LGPL-3",
     "category": "Sales",
     "depends": ["base"],
     "application": True,
+    "installable": True,
+
     "data": [
         "security/ir.model.access.csv",
         "views/estate_property_type_views.xml",
@@ -15,14 +17,9 @@
         "views/estate_property_offer_views.xml",
         "views/estate_property_views.xml",
         "views/estate_menus.xml",
-        
-        "static/description/icon.png"
-        "static/description/cover.png"
-        "static/description/index.html"
-    
     ],
+
     "demo": [
         "demo/demo.xml"
     ],
-    "installable": True,
 }
