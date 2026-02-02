fe958acb264a0d221afc406005c523a2a68f808f
Parv Prajapati <158603903+parv224@users.noreply.github.com>
2026-02-02 10:40:01 +0530

    Finalize manifest for App Store

--- __manifest__.py
+++ __manifest__.py
@@ -1,36 +1,7 @@
-# {
-#     "name": "Real Estate",
-#     "summary": "Test",
-#     "version": "18.0.0.0",
-#     "author": "Odoo PS",
-#     "website": "http://www.odoo.com",
-#     "license": "LGPL-3",
-#     "category": "Sales",
-#     "depends": ["crm","base"],
-#     "data": [
-#     "security/ir.model.access.csv",
-#
-#     # VIEWS FIRST (ACTIONS ARE HERE)
-#     "views/estate_property_type_views.xml",
-#     "views/estate_property_tags_views.xml",
-#     "views/estate_property_offer_views.xml",
-#     "views/estate_property_views.xml",
-#
-#     # MENUS LAST
-#     "views/estate_menus.xml",
-#     ],
-#
-#
-#     "demo": [
-#         "demo/demo.xml"
-#     ],
-#     "installable": True,
-# }
-
 {
     "name": "Real Estate",
     "summary": "Simple real estate property and offer management",
-    "version": "18.0.0.0",
+    "version": "18.0.1.0",
     "author": "Odoo PS",
     "website": "http://www.odoo.com",
     "license": "LGPL-3",
